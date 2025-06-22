1063ffbe83db9ea7fbdbebf201a6779cd9a651f6
turboBasic ⚡️💀 <andrii7melnyk@proton.me>
2025-06-22 19:29:17 +0200

    feat: move public functions to the main file

--- zsh-homebrew.plugin.zsh
+++ zsh-homebrew.plugin.zsh
@@ -24,3 +24,76 @@ PLUGIN_ZSH_HOMEBREW[functions]=''
     # Now when all conflicting functiones are removed we can autoload our functions
     builtin autoload -Uz $function_files
 }
+
+function homebrew_prefix() {
+    # Find Homebrew prefix without executing Homebrew, empty if not found
+
+    # Standard header of Zsh plugin function.
+    # - https://zdharma-continuum.github.io/zinit/wiki/zsh-plugin-standard/#standard_recommended_options
+    # - https://zdharma-continuum.github.io/zinit/wiki/zsh-plugin-standard/#standard_recommended_variables
+    emulate -L zsh
+    setopt \
+        extended_glob \
+        no_auto_pushd \
+        no_short_loops \
+        rc_quotes \
+        typeset_silent \
+        warn_create_global
+
+    local MATCH REPLY
+    integer MBEGIN MEND
+    local -a match mbegin mend reply
+
+    # End of standard header of Zsh plugin function
+
+
+    if [[ -n "$HOMEBREW_PREFIX" ]]; then
+        echo $HOMEBREW_PREFIX
+    elif (( $+commands[brew] )); then
+        echo ${commands[brew]:h:h}  # /opt/homebrew/bin/brew → /opt/homebrew
+    else
+        echo ${$(.homebrew_path_to_homebrew):h:h}
+    fi
+}
+
+function homebrew_path_to_gnu_utils() {
+    # Find path to Homebrew's GNU Coreutils package, empty if not found
+
+    # Standard header of Zsh plugin function.
+    # - https://zdharma-continuum.github.io/zinit/wiki/zsh-plugin-standard/#standard_recommended_options
+    # - https://zdharma-continuum.github.io/zinit/wiki/zsh-plugin-standard/#standard_recommended_variables
+
+    emulate -L zsh
+    setopt \
+        extended_glob \
+        no_auto_pushd \
+        no_short_loops \
+        rc_quotes \
+        typeset_silent \
+        warn_create_global
+
+    local MATCH REPLY
+    integer MBEGIN MEND
+    local -a match mbegin mend reply
+
+    # End of standard header of Zsh plugin function
+
+
+    local homebrew_prefix=$(homebrew_prefix)
+    # only directories and symlinks to directories  
+    echo ${homebrew_prefix:+$homebrew_prefix/opt/coreutils/libexec/gnubin}(-N/)
+}
+
+function .homebrew_path_to_homebrew() {
+    # Find path to Homebrew binary, empty if not found
+    
+    typeset -aU homebrew=(
+        $commands[brew]
+        $HOMEBREW_PREFIX/bin/brew
+        /home/linuxbrew/.linuxbrew/bin/brew
+        /opt/homebrew/bin/brew
+        /usr/local/bin/brew
+    )
+    reply=( ${^homebrew}(-*N) )  # only executable files, including symlinks to them
+    echo ${(F)reply}
+}
